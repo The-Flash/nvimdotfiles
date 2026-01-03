ba082ca39c226028fa24f4a14cc318d4a5b1b830
Augustine Smith <absaugustineflash@gmail.com>
2026-01-03 08:12:20 +0000

    feat: add split keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,13 @@ vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>")
 
+-- Splits
+vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split
+vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+-- navigate between splits
+vim.keymap.set("n", "<C-h>", "<C-w>h") -- move to left split
+vim.keymap.set("n", "<C-j>", "<C-w>j") -- move to bottom split
+vim.keymap.set("n", "<C-k>", "<C-w>k") -- move to top split
+vim.keymap.set("n", "<C-l>", "<C-w>l") -- move to right split
