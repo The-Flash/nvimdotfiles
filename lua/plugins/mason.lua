9f899f87571102b7041477ef1a8f537f050691fa
Augustine Smith <absaugustineflash@gmail.com>
2026-05-15 17:02:41 +0000

    feat: support pyright

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls" },
+			ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "pyright", "ruff" },
 		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
@@ -11,11 +11,6 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            vim.lsp.enable("lua_ls")
-            vim.lsp.enable("rust_analyzer")
-            vim.lsp.enable("ts_ls")
-            vim.lsp.enable("ruff")
-            vim.lsp.enable("pyright")
             require("mason-lspconfig").setup({
 				default_handler = function(server_name)
 					lspconfig[server_name].setup({
@@ -23,6 +18,11 @@ return {
 					})
 				end,
                 handlers = {
+                    ["pyright"] = function()
+                        lspconfig.pyright.setup({
+                            capabilities = capabilities,
+                       })
+                    end,
                     ["lua_ls"] = function()
                         lspconfig.lua_ls.setup({
                             capabilities = capabilities,
